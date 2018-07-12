483d5fdfa607d19b14a2fc481963e5b71d74d269
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-07-12 15:25:10 +1200

    Add playCards function

--- src/main/java/game/CheatGame.java
+++ src/main/java/game/CheatGame.java
@@ -1,4 +1,33 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import static util.Config.*;
+
 public class CheatGame {
+
+	private final int numPlayers;
+	private Stack<Card> pile = new Stack<>();
+
+	private int turn = 0;
+
+	public CheatGame(int numPlayers) {
+		assert numPlayers > MIN_PLAYERS;
+		assert numPlayers < MAX_PLAYERS;
+		this.numPlayers = numPlayers;
+	}
+
+	/**
+	 * Plays the cards that are passed into this function, adding them to the pile.
+	 * @param cards the cards to add.
+	 */
+	public void playCards(List<Card> cards) {
+		pile.addAll(cards);
+	}
+
+	public Stack<Card> getPile() {
+		return pile;
+	}
 }
