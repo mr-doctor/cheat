a57dc14a729d494cc8358fa4dfefafca27b676ad
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-07-12 14:28:44 +1200

    Add initial placeholder gherkin tests

---
+++ src/test/java/gherkin/TestSteps.java
@@ -0,0 +1,18 @@
+package gherkin;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.*;
+
+public class TestSteps {
+
+	@Given("^a foo$")
+	public void givenAFoo() {
+
+	}
+
+	@Then("^a bar$")
+	public void thenABar() {
+
+	}
+
+}
---
+++ src/test/resources/features/Test.feature
@@ -0,0 +1,5 @@
+Feature: foo
+
+  Scenario: bar
+	Given a foo
+	Then a bar
