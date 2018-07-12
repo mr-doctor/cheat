961ee550dc4694b91189aed3fbf58284089f1d22
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-07-12 17:53:43 +1200

    Add functionality for next turn, claiming, and playing cards

--- src/main/java/game/CheatGame.java
+++ src/main/java/game/CheatGame.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import static game.util.Util.*;
 import static util.Config.*;
 
 public class CheatGame {
 
 	private final int numPlayers;
 	private Stack<Card> pile = new Stack<>();
+	private List<Card> lastPlayed;
+	private List<Card> lastClaimed;
 
-	private int turn = 0;
+	private int turn;
 
 	public CheatGame(int numPlayers) {
 		assert numPlayers > MIN_PLAYERS;
 		assert numPlayers < MAX_PLAYERS;
 		this.numPlayers = numPlayers;
+		turn = 1;
 	}
 
 	/**
@@ -25,6 +29,77 @@ public class CheatGame {
 	 */
 	public void playCards(List<Card> cards) {
 		pile.addAll(cards);
+		lastPlayed = cards;
+	}
+
+	/**
+	 * Claims the cards that are passed into this function.
+	 * @param cards the cards to add.
+	 */
+	public void claimCards(List<Card> cards) {
+		assert !cards.isEmpty();
+		lastClaimed = cards;
+	}
+
+	public void claimCards(int number, Type type) {
+		assert number > 0;
+		assert type != null;
+		for (int i = 0; i < number; i++) {
+			lastClaimed.add(new Card(Suit.values()[(int) (Math.random() * Suit.values().length)], type));
+		}
+	}
+
+	/**
+	 * A player calls Cheat on the last-played set of cards. The player calling must not be the player who played those
+	 * cards, and the pile must have cards.
+	 *
+	 * @param playerCalling the index of the player calling Cheat
+	 * @return true if the cards were invalid and the call was correct,
+	 * false if the cards were valid and the call was incorrect.
+	 */
+	public boolean callCheat(int playerCalling) {
+		assert playerCalling != turn;
+		assert pile.size() > 0;
+
+		lastPlayed = new ArrayList<>();
+		lastClaimed = new ArrayList<>();
+
+		return !lastPlayIsValid();
+	}
+
+	public int nextTurn() {
+		turn = (turn + 1 > numPlayers) ? 1 : turn + 1;
+		return turn;
+	}
+
+	public int getTurn() {
+		return turn;
+	}
+
+	public void setTurn(int turn) {
+		this.turn = turn;
+	}
+
+	/**
+	 * Returns the pile of cards and resets the pile. Should be used when a player must pick up the pile.
+	 * @return the pile of Cards as a Stack.
+	 */
+	public Stack<Card> drawPile() {
+		Stack<Card> pile = getPile();
+		this.pile = new Stack<>();
+		return pile;
+	}
+
+	private boolean lastPlayIsValid() {
+		if (lastPlayed.size() != lastClaimed.size()) {
+			return false;
+		}
+		for (int i = 0; i < lastPlayed.size(); i++) {
+			if (!verifyCard(lastPlayed.get(i), lastClaimed.get(i))) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public Stack<Card> getPile() {
--- src/main/java/game/util/Util.java
+++ src/main/java/game/util/Util.java
@@ -1,9 +1,21 @@
 package game.util;
 
+import game.Card;
+
 public class Util {
 	public enum Suit {
 		HEARTS, DIAMONDS, SPADES, CLUBS }
 
 	public enum Type { ACE, TWO, THREE, FOUR, FIVE, SIX,
 		SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING }
+
+	/**
+	 * Checks to see if two cards have the same type.
+	 * @param card1
+	 * @param card2
+	 * @return
+	 */
+	public static boolean verifyCard(Card card1, Card card2) {
+		return card1.getType().equals(card2.getType());
+	}
 }
