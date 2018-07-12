002b9193f029f58dad449a64e311a4b45ad130df
Jacob Cohn-Gell <jacob.cohn.gell@gmail.com>
2018-07-12 18:13:02 +1200

    Lombokify Card

--- src/main/java/game/Card.java
+++ src/main/java/game/Card.java
@@ -1,24 +1,17 @@
 package game;
 
 import game.util.Util.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Card {
 
 	private final Suit suit;
 	private final Type type;
-
-	public Card(Suit suit, Type type) {
-		this.suit = suit;
-		this.type = type;
-	}
-
-	public Suit getSuit() {
-		return suit;
-	}
-
-	public Type getType() {
-		return type;
-	}
 
 	/**
 	 * A custom clone function which creates a new Card using the same suit and type as the origin object.
