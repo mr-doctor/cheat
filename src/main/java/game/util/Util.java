7ecb6de8a4277233bb145fe4c7f58d47d7bff9d2
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2018-07-12 15:25:22 +1200

    Edit enums

--- src/main/java/game/util/Util.java
+++ src/main/java/game/util/Util.java
@@ -1,7 +1,9 @@
 package game.util;
 
 public class Util {
-	public enum Suit { HEARTS, DIAMONDS, SPADES, CLUBS }
+	public enum Suit {
+		HEARTS, DIAMONDS, SPADES, CLUBS }
+
 	public enum Type { ACE, TWO, THREE, FOUR, FIVE, SIX,
 		SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING }
 }
